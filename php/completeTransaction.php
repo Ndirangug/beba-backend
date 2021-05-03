f6ae497ed20b88e888dcedc5514546fc65ffce60
George Ndirangu <ndirangu.mepawa@gmail.com>
2021-05-03 23:45:28 +0300

    add ipay php script

--- php/completeTransaction.php
+++ php/completeTransaction.php
@@ -24,8 +24,57 @@
 
 ?>
 
+<head><style>.loader {
+            animation:spin 1s infinite linear;
+            border:solid 2vmin transparent;
+            border-radius:50%;
+            border-right-color:#09f;
+            border-top-color:#09f;
+            box-sizing:border-box;
+            height:20vmin;
+            left:calc(50% - 10vmin);
+            position:fixed;
+            top:calc(50% - 10vmin);
+            width:20vmin;
+            z-index:1;
+            &:before {
+              animation:spin 2s infinite linear;
+              border:solid 2vmin transparent;
+              border-radius:50%;
+              border-right-color:#3cf;
+              border-top-color:#3cf;
+              box-sizing:border-box;
+              content:"";
+              height:16vmin;
+              left:0;
+              position:absolute;
+              top:0;
+              width:16vmin;
+            }
+            &:after {
+              animation:spin 3s infinite linear;
+              border:solid 2vmin transparent;
+              border-radius:50%;
+              border-right-color:#6ff;
+              border-top-color:#6ff;
+              box-sizing:border-box;
+              content:"";
+              height:12vmin;
+              left:2vmin;
+              position:absolute;
+              top:2vmin;
+              width:12vmin;
+            }
+          }
+          
+          @keyframes spin {
+            100% {
+              transform:rotate(360deg);
+            }
+          }</style></head>
 
-<FORM action="https://payments.ipayafrica.com/v3/ke">
+<div class="loader"/>
+<FORM name="ipay" style="display:none;" action="https://payments.ipayafrica.com/v3/ke">
 <?php  
                  foreach ($fields as $key => $value) {
                      echo $key;
@@ -37,5 +86,7 @@
 <button type="submit">  Lipa  </button>
             
 </FORM>
+
+<script>window.onload = function(){document.forms['ipay'].submit();}</script>
 
        
